f86776a9af564ac2651684f284971318c4b5060d
sigma67 <benedikt.putz@wiwi.uni-regensburg.de>
2021-12-03 20:44:44 +0100

    day 3

---
+++ src/day03.rs
@@ -0,0 +1,72 @@
+use std::str::Lines;
+
+pub fn part1(input: String) -> u32 {
+    let lines = parse_input(input);
+    let gamma_seq = compute_gamma_seq(lines);
+    let gamma = gamma_seq.iter().fold(
+        0, |gamma, &bit| (gamma << 1) ^ bit as u32
+    );
+    let delta = gamma_seq.iter().fold(
+        0, |gamma, &bit| (gamma << 1) ^ !bit as u32
+    );
+    gamma * delta
+}
+
+pub fn part2(input: String) -> u32 {
+    let lines = parse_input(input);
+
+    get_support_rating(&lines, true) * get_support_rating(&lines, false)
+}
+
+fn parse_input(input: String) -> Vec<Vec<u8>> {
+    input.lines()
+        .map(|l| l.chars()
+            .map(|c| c.to_digit(2).unwrap() as u8).collect()).collect()
+}
+
+
+fn compute_gamma_seq(lines: Vec<Vec<u8>>) -> [bool; 12]{
+    let mut counts: [u16; 12] = [0; 12];
+    let mut total: u16 = 0;
+    for line in lines {
+        for (i, c) in line.iter().enumerate() {
+            counts[i] += *c as u16
+        }
+        total += 1;
+    }
+    let seq: [bool; 12] = counts.map(|c| total - c > total / 2);
+    seq
+}
+
+fn get_support_rating(lines: &Vec<Vec<u8>>, most_common: bool) -> u32 {
+    let mut positions: [u8; 1000] = [1; 1000];
+    for i in 0..lines[0].len() {
+        let total = positions.iter().map(|&b| b as u16).sum::<u16>();
+        if total == 1 { println!{"positions {:?}", positions}; break; }
+        let mut count: u16 = 0;
+        for (j, line) in lines.iter().enumerate(){
+            count += (*line.get(i).unwrap() & positions[j]) as u16;
+        }
+        let selected: u8 = match most_common {
+            true => (count as f32  >= total as f32 / 2.0) as u8,
+            false => ((count as f32) < total as f32 / 2.0) as u8
+        };
+        println!("selected is {}. 0 - {}, 1 - {}", selected, total - count, count);
+
+        for (j, line) in lines.iter().enumerate() {
+            positions[j] = positions[j] & (line[i] == selected) as u8;
+        }
+    }
+    let mut rating: u32 = 0;
+    for (k, p) in positions.iter().enumerate(){
+        if *p == 1 as u8 {
+            println!("{:?}", lines[k]);
+            rating = lines[k].iter().fold(
+                0, |gamma, &bit| (gamma << 1) ^ bit as u32
+            );
+            println!("{}", rating)
+        }
+    }
+    rating
+}
+
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,9 @@
+pub mod submarine;
+
 // Days
 pub mod day01;
 pub mod day02;
-pub mod submarine;
+pub mod day03;
 
 pub fn noop(_inp: String) -> u32 { return 0 }
 
@@ -11,6 +13,7 @@ pub fn get_day(day: u32) -> (DayFn, DayFn) {
     return match day {
         1 => (day01::part1, day01::part2),
         2 => (day02::part1, day02::part2),
+        3 => (day03::part1, day03::part2),
         _ => {
             println!("Unknown day: {}", day);
             return (noop, noop);
