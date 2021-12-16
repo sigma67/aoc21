08ebc3208955d941eee2cc2024f58e3a6e18032e
sigma67 <benedikt.putz@wiwi.uni-regensburg.de>
2021-12-16 23:07:46 +0100

    day 16 part 2

--- src/day16.rs
+++ src/day16.rs
@@ -1,62 +1,72 @@
 use std::iter::Peekable;
 use std::vec::IntoIter;
 
-struct Packet {
-    version: u8,
-    type_id: u8
-}
-
 pub fn part1(input: String) -> u64 {
-    decode(input)
+    decode(&input, true)
 }
 
 pub fn part2(input: String) -> u64 {
-    0
+    decode(&input, false)
 }
 
-pub fn decode(input: String) -> u64{
+pub fn decode(input: &str, version: bool) -> u64{
     let hex = hex_to_bits(&input.trim());
     let mut iter = hex.into_iter().peekable();
-    let mut packets:Vec<Packet> = Vec::new();
     let mut version_sum = 0;
     let mut depth = 0;
-    let mut depth_mode: Vec<i8> = vec![-1; 100];
-    let mut depth_remaining: Vec<u64> = vec![0; 100];
+    let mut depth_mode: Vec<i8> = vec![-1; input.len() % 100];
+    let mut depth_remaining: Vec<u64> = vec![0; input.len() % 100];
     let i = iter.by_ref();
+    let mut ops:Vec<u8> = Vec::new();
+    let mut vals:Vec<Vec<u64>> = vec![Vec::new(); input.len() % 100];
     while i.peek().is_some(){
         let version = to_num(take_n(i, 3));
         version_sum += version;
-        let type_id = to_num(take_n(i, 3));
-        let mut content;
+        let type_id = to_num(take_n(i, 3)) as u8;
         if type_id == 4 {
             let mut four = take_n(i, 5);
             let mut bit_count = 11;
             let mut literal:Vec<u8> = Vec::new();
             while four[0] == 1 {
-                literal.extend(&four[1..4]);
+                literal.extend(&four[1..5]);
                 four = take_n(i, 5);
                 bit_count += 5;
             }
-            literal.extend(four);
-            reduce_remaining(depth, &depth_mode, &mut depth_remaining, bit_count, 0);
-            if depth_remaining[depth - 1] == 0 { depth = depth_remaining.iter().enumerate().filter(|(_, x) | **x!= 0).max_by(|(i, _), (j, _)| i.cmp(j)).unwrap().0 + 1}
+            literal.extend(&four[1..5]);
+            let lit = to_num(literal);
+            vals[depth].push(lit);
+            reduce_remaining(depth, &depth_mode, &mut depth_remaining, bit_count);
+            if depth_remaining[depth - 1] == 0 {
+                let depth_index = depth_remaining.iter().enumerate()
+                    .filter(|(_, x) | **x!= 0)
+                    .max_by(|(i, _), (j, _)| i.cmp(j));
+                let depth_new = match depth_index {
+                    Some(x) => x.0 + 1,
+                    None => 0
+                };
+                for j in 0..depth-depth_new {
+                    let mut num = reduce_ops(ops.pop().unwrap(), &vals[depth - j]);
+                    vals[depth - j] = Vec::new();
+                    vals[depth - j - 1].push(num);
+                }
+                depth = depth_new;
+            }
             if depth == 0 || depth_remaining.iter().sum::<u64>() == 0 { break; }
-            println!("lit, depth {}, remaining {} ", depth, depth_remaining[depth - 1]);
-            content = to_num(literal)
         }
         else {
+            ops.push(type_id);
             let mut bit_count = 7;
-            depth += 1;
-            depth_mode[depth - 1] = take_n(i, 1)[0] as i8;
-            depth_remaining[depth - 1] = match depth_mode[depth - 1] {
+            depth_mode[depth] = take_n(i, 1)[0] as i8;
+            let remaining = match depth_mode[depth] {
                 0 => {bit_count += 15; to_num(take_n(i, 15)) },
                 _ => {bit_count += 11; to_num(take_n(i, 11)) }
             };
-            println!("op, depth {}, remaining {} ", depth, depth_remaining[depth - 1]);
-            reduce_remaining(depth, &depth_mode, &mut depth_remaining, bit_count, 1);
+            reduce_remaining(depth, &depth_mode, &mut depth_remaining, bit_count);
+            depth_remaining[depth] = remaining;
+            depth += 1;
         }
     }
-    version_sum
+    if version { version_sum } else { vals[0][0] }
 }
 
 
@@ -82,9 +92,22 @@ fn take_n(iter: &mut Peekable<IntoIter<u8>>, count: usize) -> Vec<u8> {
     iter.take(count).collect::<Vec<_>>()
 }
 
-fn reduce_remaining(depth: usize, depth_mode: &Vec<i8>, depth_remaining: &mut Vec<u64>, by: u64, reduce: usize){
-    for i in 0..depth - reduce {
+fn reduce_remaining(depth: usize, depth_mode: &Vec<i8>, depth_remaining: &mut Vec<u64>, by: u64){
+    for i in 0..depth {
         if depth_mode[i] == 0 { depth_remaining[i] -= by; }
     }
-    if depth_mode[depth - 1] == 1 && depth_remaining[depth - 1] > 0 { depth_remaining[depth - 1] -= 1; }
+    if depth > 0 && depth_mode[depth - 1] == 1 && depth_remaining[depth - 1] > 0 { depth_remaining[depth - 1] -= 1; }
+}
+
+fn reduce_ops(type_id: u8, vals: &Vec<u64>) -> u64 {
+    match type_id {
+        0 => vals.iter().sum(),
+        1 => vals.iter().product(),
+        2 => *vals.iter().min().unwrap(),
+        3 => *vals.iter().max().unwrap(),
+        5 => if vals[0] > vals[1] { 1 } else {0},
+        6 => if vals[0] < vals[1] { 1 } else {0},
+        7 => if vals[0] == vals[1]{ 1 } else {0},
+        _ => panic!("that's not right")
+    }
 }
--- tests/day16.rs
+++ tests/day16.rs
@@ -1,15 +1,37 @@
 use advent_of_code::day16::{to_num, decode};
 
+const INPUT1: &str = "8A004A801A8002F478";
+const INPUT2: &str = "620080001611562C8802118E34";
+const INPUT3: &str = "C0015000016115A2E0802F182340";
+const INPUT4: &str = "A0016C880162017C3686B18A3D4780";
+const INPUT5: &str = "C200B40A82";
+const INPUT6: &str = "04005AC33890";
+const INPUT7: &str = "880086C3E88112";
+const INPUT8: &str = "CE00C43D881120";
+const INPUT9: &str = "D8005AC2A8F0";
+const INPUT10: &str = "F600BC2D8F";
+const INPUT11: &str = "9C005AC2F8F0";
+const INPUT12: &str = "9C0141080250320F1802104A08";
+
+
 #[test]
 fn test_day16_p1() {
-    let input = String::from("8A004A801A8002F478");
-    assert_eq!(decode(input), 16);
-    let mut input = String::from("620080001611562C8802118E34");
-    assert_eq!(decode(input), 12);
-    input = String::from("C0015000016115A2E0802F182340");
-    assert_eq!(decode(input), 23);
-    input = String::from("A0016C880162017C3686B18A3D4780");
-    assert_eq!(decode(input), 31);
+    assert_eq!(decode(INPUT1, true), 16);
+    assert_eq!(decode(INPUT2, true), 12);
+    assert_eq!(decode(INPUT3, true), 23);
+    assert_eq!(decode(INPUT4, true), 31);
+}
+
+#[test]
+fn test_day16_p2() {
+    assert_eq!(decode(INPUT5, false), 3);
+    assert_eq!(decode(INPUT6, false), 54);
+    assert_eq!(decode(INPUT7, false), 7);
+    assert_eq!(decode(INPUT8, false), 9);
+    assert_eq!(decode(INPUT9, false), 1);
+    assert_eq!(decode(INPUT10, false), 0);
+    assert_eq!(decode(INPUT11, false), 0);
+    assert_eq!(decode(INPUT12, false), 1);
 }
 
 #[test]
