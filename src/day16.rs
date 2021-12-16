fb95595e99ce24f3fd13f1c3c89739fcffecaade
sigma67 <benedikt.putz@wiwi.uni-regensburg.de>
2021-12-16 23:33:34 +0100

    day 16 opt

--- src/day16.rs
+++ src/day16.rs
@@ -1,4 +1,3 @@
-use std::iter::Peekable;
 use std::vec::IntoIter;
 
 pub fn part1(input: String) -> u64 {
@@ -11,57 +10,47 @@ pub fn part2(input: String) -> u64 {
 
 pub fn decode(input: &str, version: bool) -> u64{
     let hex = hex_to_bits(&input.trim());
-    let mut iter = hex.into_iter().peekable();
     let mut version_sum = 0;
     let mut depth = 0;
-    let mut depth_mode: Vec<i8> = vec![-1; input.len() % 100];
-    let mut depth_remaining: Vec<u64> = vec![0; input.len() % 100];
+    let len = input.len() % 100;
+    let mut depth_mode: Vec<i8> = vec![-1; len];
+    let mut depth_remaining: Vec<u64> = vec![0; len];
+    let mut vals:Vec<Vec<u64>> = vec![Vec::new(); len];
+    let mut iter = hex.into_iter();
     let i = iter.by_ref();
     let mut ops:Vec<u8> = Vec::new();
-    let mut vals:Vec<Vec<u64>> = vec![Vec::new(); input.len() % 100];
-    while i.peek().is_some(){
-        let version = to_num(take_n(i, 3));
+    loop {
+        let mut bit_size: usize = 0;
+        let bs = &mut bit_size;
+        let version = to_num(take_n(i, 3, bs));
         version_sum += version;
-        let type_id = to_num(take_n(i, 3)) as u8;
+        let type_id = to_num(take_n(i, 3, bs)) as u8;
         if type_id == 4 {
-            let mut four = take_n(i, 5);
-            let mut bit_count = 11;
+            let mut four = take_n(i, 5, bs);
             let mut literal:Vec<u8> = Vec::new();
             while four[0] == 1 {
                 literal.extend(&four[1..5]);
-                four = take_n(i, 5);
-                bit_count += 5;
+                four = take_n(i, 5, bs);
             }
             literal.extend(&four[1..5]);
-            let lit = to_num(literal);
-            vals[depth].push(lit);
-            reduce_remaining(depth, &depth_mode, &mut depth_remaining, bit_count);
-            if depth_remaining[depth - 1] == 0 {
-                let depth_index = depth_remaining.iter().enumerate()
-                    .filter(|(_, x) | **x!= 0)
-                    .max_by(|(i, _), (j, _)| i.cmp(j));
-                let depth_new = match depth_index {
-                    Some(x) => x.0 + 1,
-                    None => 0
-                };
-                for j in 0..depth-depth_new {
-                    let mut num = reduce_ops(ops.pop().unwrap(), &vals[depth - j]);
-                    vals[depth - j] = Vec::new();
-                    vals[depth - j - 1].push(num);
-                }
-                depth = depth_new;
+            vals[depth].push(to_num(literal));
+            reduce_remaining(depth, &depth_mode, &mut depth_remaining, *bs as u64);
+            while depth > 0 && depth_remaining[depth - 1] == 0 {
+                let num = reduce_ops(ops.pop().unwrap(), &vals[depth]);
+                vals[depth] = Vec::new();
+                vals[depth - 1].push(num);
+                depth -= 1;
             }
-            if depth == 0 || depth_remaining.iter().sum::<u64>() == 0 { break; }
+            if depth == 0 { break; }
         }
         else {
             ops.push(type_id);
-            let mut bit_count = 7;
-            depth_mode[depth] = take_n(i, 1)[0] as i8;
+            depth_mode[depth] = take_n(i, 1, bs)[0] as i8;
             let remaining = match depth_mode[depth] {
-                0 => {bit_count += 15; to_num(take_n(i, 15)) },
-                _ => {bit_count += 11; to_num(take_n(i, 11)) }
+                0 => to_num(take_n(i, 15, bs)),
+                _ => to_num(take_n(i, 11, bs))
             };
-            reduce_remaining(depth, &depth_mode, &mut depth_remaining, bit_count);
+            reduce_remaining(depth, &depth_mode, &mut depth_remaining, *bs as u64);
             depth_remaining[depth] = remaining;
             depth += 1;
         }
@@ -88,7 +77,8 @@ pub fn hex_to_bits(input: &str) -> Vec<u8> {
     bit_vec
 }
 
-fn take_n(iter: &mut Peekable<IntoIter<u8>>, count: usize) -> Vec<u8> {
+fn take_n(iter: &mut IntoIter<u8>, count: usize, bit_size: &mut usize) -> Vec<u8> {
+    *bit_size += count;
     iter.take(count).collect::<Vec<_>>()
 }
 
